f6bcf6635a6e76ceace2f81d1758d3075be9388e
Bar Lis <48059962+bar2693lis@users.noreply.github.com>
2023-11-19 16:15:30 +0200

    first commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,16 +3,20 @@ import "@styles/globals.css";
 import Nav from "@components/Nav";
 import Footer from "@components/Footer";
 import Provider from "@components/Provider";
+// import Favicon from '/public/images/Metadata/favicon.ico';
+import Favicon from "@public/assets/favicon.ico";
 
 export const metadata = {
   title: "Promptopia",
   description: "Dicover & Share AI Prompts",
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
+        <link rel="icon" href="./favicon.ico" sizes="any" />
         <Provider>
           <div className="main">
             <div className="gradient"></div>
